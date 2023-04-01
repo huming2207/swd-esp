2ee7d8a063b997308a29f5f8f743b22278ffd1ca
Jackson Ming Hu <huming2207@gmail.com>
2023-04-01 17:34:01 +1100

    swd: add temporary workaround to match impedance

--- cmsis_dap/DAP_config.h
+++ cmsis_dap/DAP_config.h
@@ -12,6 +12,7 @@
 #include <sdkconfig.h>
 #include <driver/gpio.h>
 #include <hal/gpio_ll.h>
+#include <esp_rom_sys.h>
 
 #if defined(CONFIG_IDF_TARGET_ESP32S2)
 #define CPU_CLOCK               CONFIG_ESP32S2_DEFAULT_CPU_FREQ_MHZ * 1000000        ///< Specifies the CPU Clock in Hz
@@ -100,12 +101,17 @@ static inline void PORT_JTAG_SETUP(void)
 
 static inline void PORT_SWD_SETUP(void)
 {
+    // FIXME: later on Rev. 3.1 this should be removed
+    gpio_set_drive_capability(PIN_SWCLK, GPIO_DRIVE_CAP_1);
+    gpio_set_drive_capability(PIN_SWDIO, GPIO_DRIVE_CAP_2);
+
     // Set SWCLK HIGH, pull-up only
     gpio_ll_output_enable(&GPIO, PIN_SWCLK);
     gpio_ll_od_disable(&GPIO, PIN_SWCLK);
     gpio_ll_set_level(&GPIO, PIN_SWCLK, 1);
     gpio_ll_pulldown_dis(&GPIO, PIN_SWCLK);
     gpio_ll_pullup_en(&GPIO, PIN_SWCLK);
+    gpio_ll_pin_filter_disable(&GPIO, PIN_SWCLK);
 
 
     // Set SWDIO HIGH, pull-up only
@@ -114,6 +120,7 @@ static inline void PORT_SWD_SETUP(void)
     gpio_ll_set_level(&GPIO, PIN_SWDIO, 1);
     gpio_ll_pulldown_dis(&GPIO, PIN_SWDIO);
     gpio_ll_pullup_en(&GPIO, PIN_SWDIO);
+    gpio_ll_pin_filter_disable(&GPIO, PIN_SWDIO);
 
     // Set RESET HIGHm pull-up only
     gpio_ll_output_enable(&GPIO, PIN_nRST);
